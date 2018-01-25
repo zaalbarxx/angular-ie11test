aad43baac9fec0befe67d4a0e20da4a561a31d05
Maksym Cierżniak <maksym.cierzniak@gmail.com>
2018-01-25 10:50:53 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NgForageConfig, NgForage} from '@ngforage/ngforage-ng5';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private config: NgForageConfig, store: NgForage) {
+  }
 }
